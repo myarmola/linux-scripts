667ace218f43ab18faf76b655394fd26241ec0e9
myarmola <myarmola@icloud.com>
2025-08-06 02:02:54 +0300

    improve script

--- simple-archiving-tool.sh
+++ simple-archiving-tool.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 if [ ! -d "$1" ]; then
-    echo "The directory was not specified or does not exist" # To make sure the argument is a directory
+    echo "The directory was not specified or does not exist"
     exit 1
 fi
 
-archive="$HOME/logs_archive_$(date +"%F_%H-%M-%S").tar.gz" # It's a default path, you may change to your own
-tar czf "$archive" --absolute-names "$1" 
+archived_dir=$(basename "$1")
+destination="$HOME/logs_archives" # Default destination for archives, can be changed
+archive="$destination/${archived_dir}_logs_archive_$(date +"%F_%H-%M-%S").tar.gz"
+mkdir -p "$destination"
+tar czf "$archive" -C "$1" .
+
 if [ $? -eq 0 ]; then
-    echo "The archive $archive has been created"
+    echo "The archive $archive has been created successfully."
 fi
