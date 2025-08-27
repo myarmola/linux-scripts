d69858e6632b8ad4e3bea4c7cbde33ff30fbb42d
myarmola <myarmola@icloud.com>
2025-08-28 01:13:50 +0300

    syntax fix

--- nginx-log-analyzer.sh
+++ nginx-log-analyzer.sh
@@ -6,8 +6,8 @@ if [ ! -f "$1" ]; then
 fi
 log="$1"
 echo "Top 5 IP addresses with the most requests:"
-grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" "$log" | sort | uniq -c | sort -nr | head -n 5 | awk '{print "From address " $2 " - " $1 " requests"}'
+grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $log | sort | uniq -c | sort -nr | head -n 5 | awk '{print "From address " $2 " - " $1 " requests"}'
 echo -e "\nTop 5 most requested paths:"
-grep -E -o "(GET|POST|PUT|DELETE) /[^ ]+" "$log" | sort | uniq -c | sort -nr | head -n 5 | awk '{print "To path " $3 " - " $1 " requests, method - " $2}'
+grep -E -o "(GET|POST|PUT|DELETE) /[^ ]+" $log | sort | uniq -c | sort -nr | head -n 5 | awk '{print "To path " $3 " - " $1 " requests, method - " $2}'
 echo -e "\nTop 5 response status codes:"
 grep -E -o '" [0-9]{3}' $log | sort | uniq -c | sort -nr | head -n 5 | awk '{print "Status code " $3 " - " $1 " requests"}'
